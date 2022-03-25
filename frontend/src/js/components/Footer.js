0035f15462f2116de33c4ebe8f335f4cd9c98230
Darius Hammond <hammond.work.business@gmail.com>
2022-03-25 12:50:31 -0400

    footerUpdate

--- frontend/src/js/components/Footer.js
+++ frontend/src/js/components/Footer.js
@@ -9,16 +9,16 @@ function SetupFooter(){
  
 
     return `
-        <div>
+        <div class="grid-container" >
         
         
-        <section class="Links">
-       <ul><a href="https://github.com/BradRWeir" target="_blank">Link to Brad's Github</a> </ul>
-       <ul><a href="https://github.com/DenzaleM" target="_blank"> Denzale's Github Link</a> </ul>
-       <ul><a href="http://github.com/adia1bug"target="_blank" >Qadriyyah's Github Link </a> </ul>
-       <ul><a href="https://www.linkedin.com/in/jessica-yuting-wang/"target="_blank" >Jessica's linkedin</a> </ul>
-       <ul><a href="https://www.linkedin.com/in/darius-hmmond/" target="_blank">Daruis linkedin </a> </ul>
-       <ul><a href="https://www.linkedin.com/in/rimma-girsheva" target="_blank">Rimma's linkedin </a> </ul>
+        <section class="Links grid-container" >
+         <a href="https://github.com/BradRWeir" target="_blank">Link to Brad's Github</a> 
+         <a href="https://github.com/DenzaleM" target="_blank"> Denzale's Github Link</a> 
+         <a href="http://github.com/adia1bug"target="_blank" >Qadriyyah's Github Link </a> 
+       <a href="https://www.linkedin.com/in/jessica-yuting-wang/"target="_blank" >Jessica's linkedin</a>
+       <a href="https://www.linkedin.com/in/darius-hmmond/" target="_blank">Daruis linkedin </a> 
+       <a href="https://www.linkedin.com/in/rimma-girsheva" target="_blank">Rimma's linkedin </a> 
         </section>
 
             <p>&copy; 2022   </p>
